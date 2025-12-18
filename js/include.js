eb6e676ab3d544e9a91bcb8be6db12b7bb598875
codecortex101-tech <codecortex101@gmail.com>
2025-12-18 12:40:52 +0500

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,7 +1,7 @@
 fetch('./header.html')
-  .then(r => r.text())
-  .then(d => document.getElementById('site-header').innerHTML = d);
+.then(r => r.text())
+.then(d => document.getElementById('site-header').innerHTML = d);
 
 fetch('./footer.html')
-  .then(r => r.text())
-  .then(d => document.getElementById('site-footer').innerHTML = d);
+.then(r => r.text())
+.then(d => document.getElementById('site-footer').innerHTML = d);
